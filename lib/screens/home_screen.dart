8d8bfa3e9a1f0d4b78a4f1a38e9f7620a570d12e
VanshKela <vanshkela2@gmail.com>
2021-05-09 17:40:11 +0530

    back_button figured

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,15 +12,65 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Future<bool> _onBackPressed() {
+    return showDialog(
+      context: context,
+      builder: (context) => new AlertDialog(
+        title: new Text('Are you sure?'),
+        content: new Text('Do you want to exit an App'),
+        actions: <Widget>[
+          new GestureDetector(
+            onTap: () => Navigator.of(context).pop(false),
+            child: roundedButton("No", const Color(0xFF167F67),
+                const Color(0xFFFFFFFF)),
+          ),
+          new GestureDetector(
+            onTap: () => Navigator.of(context).pop(true),
+            child: roundedButton(" Yes ", const Color(0xFF167F67),
+                const Color(0xFFFFFFFF)),
+          ),
+        ],
+      ),
+    ) ??
+        false;
+  }
+
+  Widget roundedButton(String buttonLabel, Color bgColor, Color textColor) {
+    var loginBtn = new Container(
+      padding: EdgeInsets.all(5.0),
+      alignment: FractionalOffset.center,
+      decoration: new BoxDecoration(
+        color: bgColor,
+        borderRadius: new BorderRadius.all(const Radius.circular(10.0)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: const Color(0xFF696969),
+            offset: Offset(1.0, 6.0),
+            blurRadius: 0.001,
+          ),
+        ],
+      ),
+      child: Text(
+        buttonLabel,
+        style: new TextStyle(
+            color: textColor, fontSize: 20.0, fontWeight: FontWeight.bold),
+      ),
+    );
+    return loginBtn;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Stack(
-          children: [
-            DrawerScreen(),
-            MyApp(),
-          ],
+    return WillPopScope(
+      onWillPop: _onBackPressed,
+      child: MaterialApp(
+        home: Scaffold(
+          body: Stack(
+            children: [
+              DrawerScreen(),
+              MyApp(),
+            ],
+          ),
         ),
       ),
     );
